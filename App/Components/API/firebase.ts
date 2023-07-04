a2559a62fd2469ee7c0c196374e06fbc04799da5
Naeem Carr <naeem@liquidthought.co.za>
2023-07-04 14:03:03 +0200

    added types for env

--- App/Components/API/firebase.ts
+++ App/Components/API/firebase.ts
@@ -1,17 +1,22 @@
+import {getFirestore, Firestore} from 'firebase/firestore';
 import {initializeApp, getApps} from 'firebase/app';
 import {getAuth, Auth} from 'firebase/auth';
-import {getFirestore, Firestore} from 'firebase/firestore';
-import {config} from 'dotenv';
-
-config();
+import {
+  API_KEY,
+  APP_ID,
+  PROJECT_ID,
+  AUTH_DOMAIN,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
